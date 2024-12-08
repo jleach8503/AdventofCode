76e271e4fc3f1b930d9f9d18bb4026be45e777db
jleach8503 <jonathanm.leach@gmail.com>
2024-12-08 20:31:04 +0000

    2015 Day 4 completed!

--- 2015/04/Solution.ps1
+++ 2015/04/Solution.ps1
@@ -9,6 +9,7 @@ try {
     $md5 = [System.Security.Cryptography.MD5]::Create()
     $result = 0
     while ($true) {
+        Write-Host ("Currently processing: {0}`r" -f $result) -NoNewline
         $secretKey = $puzzleInput + $result
         $byteArray = [System.Text.Encoding]::UTF8.GetBytes($secretKey)
         $hashBytes = $md5.ComputeHash($byteArray)
@@ -20,16 +21,37 @@ try {
     }
 }
 finally {
-    Write-Host ('Lowest Positive Number: {0}' -f $result) -ForegroundColor Green
+    Write-Host ('Lowest Positive Number with 5 zeros: {0}' -f $result) -ForegroundColor Green
     Write-Host "Elapsed Time: $($stopwatch.ElapsedMilliseconds) ms"
     Write-Host "Actions Performed: $result"
+    $stopwatch.Stop()
 }
 
 #endregion
 
 #region Part 2
-
-
-
-Write-Host ('Total Ribbon Required: {0}' -f $result) -ForegroundColor Green
+try {
+    $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+    Clear-Host
+    while ($true) {
+        if ($result % 100000 -eq 0) {
+            Write-Host ("`rCurrently processing: {0}" -f $result) -NoNewline
+        }
+        $secretKey = [System.String]::Concat($puzzleInput,$result)
+        $byteArray = [System.Text.Encoding]::UTF8.GetBytes($secretKey)
+        $hashBytes = $md5.ComputeHash($byteArray)
+        $isMatch = ($hashBytes[0] -eq 0 -and $hashBytes[1] -eq 0 -and $hashBytes[2] -eq 0)
+        if ($isMatch) {
+            Write-Host ''
+            break
+        }
+        $result++
+    }
+}
+finally {
+    Write-Host ('Lowest Positive Number with 6 zeros: {0}' -f $result) -ForegroundColor Green
+    Write-Host "Elapsed Time: $($stopwatch.ElapsedMilliseconds) ms"
+    Write-Host "Actions Performed: $result"
+    $stopwatch.Stop()
+}
 #endregion
