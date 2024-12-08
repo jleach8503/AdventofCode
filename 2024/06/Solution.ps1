39868ad07124e1cdfaeee98c133bf921c711f3b5
jleach8503 <jonathanm.leach@gmail.com>
2024-12-08 17:30:19 +0000

    Day 6 Part 1 finished... part 2 looks hard

--- 2024/06/Solution.ps1
+++ 2024/06/Solution.ps1
@@ -90,118 +90,220 @@ Predict the path of the guard. How many distinct positions will the guard visit 
 #>
 
 $puzzleInput = Get-Content -Path '.\input.txt'
-$map = $puzzleInput | ForEach-Object { ,$_.ToCharArray() }
-
-# Get the starting position
-$yPos = $puzzleInput.IndexOf(($puzzleInput | Where-Object { $_ -like '*^*' }))
-$xPos = $puzzleInput[$yPos].IndexOf('^')
-$guard = [pscustomobject]@{
-    Direction = '^'
-    X = $xPos
-    Y = $yPos
-}
 
 function Move-Guard {
     [CmdletBinding()]
     param (
-        [pscustomobject]$Guard,
-        $Map
-    )
 
+    )
     # Set the current location to visited
-    $Map[$Guard.X][$Guard.Y] = 'X'
+    $map["$($Guard.Row),$($Guard.Col)"] = 'X'
 
     # Get the next coordinate
     $nextCoords = switch ($Guard.Direction) {
         '^' {
             @(
-                $Guard.X
-                $Guard.Y-1
+                $Guard.Row-1
+                $Guard.Col
             )
         }
         '>' {
             @(
-                $Guard.X+1
-                $Guard.Y
+                $Guard.Row
+                $Guard.Col+1
             )
         }
         'V' {
             @(
-                $Guard.X
-                $Guard.Y+1
+                $Guard.Row+1
+                $Guard.Col
             )
         }
         '<' {
             @(
-                $Guard.X-1
-                $Guard.Y
+                $Guard.Row
+                $Guard.Col-1
             )
         }
     }
 
-    $nextValue = $Map[$nextCoords[0]][$nextCoords[1]]
+    $nextValue = $Map["$($nextCoords[0]),$($nextCoords[1])"]
     if ($nextValue -eq '#') {
-        switch ($Guard.Direction) {
-            '^' {
-                $Guard.Direction = '>'
-            }
-            '>' {
-                $Guard.Direction = 'V'
-            }
-            'V' {
-                $Guard.Direction = '<'
-            }
-            '<' {
-                $Guard.Direction = '^'
-            }
-        }
+        $Guard.Direction = $script:changeDirection[$Guard.Direction]
         Write-Verbose -Message ('Guard has changed directions to {0}!' -f $Guard.Direction)
     }
-    elseif ($null -eq $nextValue) {
-        Write-Verbose -Message 'Guard has left the area!'
-        return $false
+    elseif ($null -ne $nextValue) {
+        $Guard.Row = $nextCoords[0]
+        $Guard.Col = $nextCoords[1]
+        Write-Verbose -Message ('Guard has moved to {0},{1}!' -f $Guard.Row,$Guard.Col)
     }
     else {
-        $Guard.X = $nextCoords[0]
-        $Guard.Y = $nextCoords[1]
-        Write-Verbose -Message ('Guard has moved to {0},{1}!' -f $Guard.X,$Guard.Y)
+        Write-Verbose -Message 'Guard has left the area!'
+        return $false
     }
 
     return $true
 }
 
 function Show-Map {
-    param (
-        $Map
-    )
+    $colorMap = @{
+        '.' = 'White'
+        'X' = 'Green'
+        '#' = 'Cyan'
+    }
 
-    for ($i = 0; $i -lt $Map.Count; $i++) {
-        for ($j = 0; $j -lt $Map[$i].Count; $j++) {
-            $writeArgs = @{
-                NoNewline = $j -lt $Map[$i].Count-1
+    for ($row = 0; $row -lt $puzzleInput.Count; $row++) {
+        for ($col = 0; $col -lt $puzzleInput[$row].Length; $col++) {
+            $value = $script:map["$row,$col"]
+            Write-Host $value -NoNewline -ForegroundColor $colorMap["$value"]
+        }
+        Write-Host ''
+    }
+}
+
+function New-Map {
+    param (
+        $PuzzleInput
+    )
+    # Create the map as a hashtable
+    $script:map = @{}
+    for ($row = 0; $row -lt $PuzzleInput.Count; $row++) {
+        for ($col = 0; $col -lt $PuzzleInput[$row].Length; $col++) {
+            $map["$row,$col"] = $PuzzleInput[$row][$col]
+            if ($PuzzleInput[$row][$col] -eq '^') {
+                $script:startingCoords = @($row,$col)
             }
-            if ($Map[$i][$j] -eq '#') {
-                $writeArgs.ForegroundColor = 'Cyan'
-            }
-            elseif ($Map[$i][$j] -eq 'X') {
-                $writeArgs.ForegroundColor = 'Green'
-            }
-            Write-Host @writeArgs $Map[$i][$j]
         }
     }
 }
 
-do {
-    $moved = Move-Guard -Guard $guard -Map $map
-} while ($moved)
+# Create the hash map of coordinates
+New-Map -PuzzleInput $puzzleInput
 
-$result = 0
-for ($i = 0; $i -lt $Map.Count; $i++) {
-    for ($j = 0; $j -lt $Map[$i].Count; $j++) {
-        if ($Map[$i][$j] -eq 'X') {
+# Create the guard
+$guard = [pscustomobject]@{
+    Direction = '^'
+    Row = $startingCoords[0]
+    Col = $startingCoords[1]
+}
+
+# Changing directions
+$script:changeDirection = @{
+    '^' = '>'
+    '>' = 'V'
+    'V' = '<'
+    '<' = '^'
+}
+
+try {
+    $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+    $actions = 0
+    do {
+        $moved = Move-Guard
+        $actions++
+    } while ($moved)
+
+    $result = 0
+    $map.GetEnumerator() | ForEach-Object {
+        if ($_.Value -eq 'X') {
             $result++
         }
     }
 }
+finally {
+    $stopwatch.Stop()
+    Write-Host ('Distinct Guard Positions: {0}' -f $result) -ForegroundColor Green
+    Write-Host "Elapsed Time: $($stopwatch.ElapsedMilliseconds) ms"
+    Write-Host "Actions Performed: $actions"
+}
 
-Write-Host ('Distinct Guard Positions: {0}' -f $result) -ForegroundColor Green
+<#
+--- Part Two ---
+While The Historians begin working around the guard's patrol route, you borrow their fancy device and step outside the lab. From the safety of a supply closet, you time travel through the last few months and record the nightly status of the lab's guard post on the walls of the closet.
+
+Returning after what seems like only a few seconds to The Historians, they explain that the guard's patrol area is simply too large for them to safely search the lab without getting caught.
+
+Fortunately, they are pretty sure that adding a single new obstruction won't cause a time paradox. They'd like to place the new obstruction in such a way that the guard will get stuck in a loop, making the rest of the lab safe to search.
+
+To have the lowest chance of creating a time paradox, The Historians would like to know all of the possible positions for such an obstruction. The new obstruction can't be placed at the guard's starting position - the guard is there right now and would notice.
+
+In the above example, there are only 6 different positions where a new obstruction would cause the guard to get stuck in a loop. The diagrams of these six situations use O to mark the new obstruction, | to show a position where the guard moves up/down, - to show a position where the guard moves left/right, and + to show a position where the guard moves both up/down and left/right.
+
+Option one, put a printing press next to the guard's starting position:
+
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+....|..#|.
+....|...|.
+.#.O^---+.
+........#.
+#.........
+......#...
+Option two, put a stack of failed suit prototypes in the bottom right quadrant of the mapped area:
+
+
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+......O.#.
+#.........
+......#...
+Option three, put a crate of chimney-squeeze prototype fabric next to the standing desk in the bottom right quadrant:
+
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+.+----+O#.
+#+----+...
+......#...
+Option four, put an alchemical retroencabulator near the bottom left corner:
+
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+..|...|.#.
+#O+---+...
+......#...
+Option five, put the alchemical retroencabulator a bit to the right instead:
+
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+....|.|.#.
+#..O+-+...
+......#...
+Option six, put a tank of sovereign glue right next to the tank of universal solvent:
+
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+.+----++#.
+#+----++..
+......#O..
+It doesn't really matter what you choose to use as an obstacle so long as you and The Historians can put it into position without the guard noticing. The important thing is having enough options that you can find one that minimizes time paradoxes, and in this example, there are 6 different positions you could choose.
+
+You need to get the guard stuck in a loop by adding a single new obstruction. How many different positions could you choose for this obstruction?
+#>
+
